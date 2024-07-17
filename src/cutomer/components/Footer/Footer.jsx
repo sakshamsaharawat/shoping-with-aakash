aa01159995224b885ce07b799d2150c68d475a3d
sakshamsaharawat <sakshamjaat88@gmail.com>
2024-07-17 23:11:08 +0530

    fix text-size in footer

--- src/cutomer/components/Footer/Footer.jsx
+++ src/cutomer/components/Footer/Footer.jsx
@@ -5,20 +5,20 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <div style={{ border: "2px solid red" }}>
+        <div>
             <Grid className='bg-black text-white text-center t-10 cursor-pointer'
                 container
                 sx={{ bgcolor: "black", color: "white", py: 3 }}
             >
                 <Grid item xs={12} sm={6} md={3}>
-                    <Typography className='pb-5 capitalize' varient="h6"> Company</Typography>
+                    <Typography className='pb-5 capitalize' style={{ fontSize: "2rem" }} varient="h2"> Company</Typography>
                     <div> <Button className='pb-5 capitalize' variant="h6" gutterBottom style={{ textTransform: "capitalize" }}> About</Button></div>
                     <div><Button className='pb-5 capitalize ' variant="h6" gutterBottom> Blog</Button></div>
                     <div> <Button className='pb-5 capitalize' variant="h6" gutterBottom> Press</Button></div>
                     <div><Button className='pb-5 capitalize' variant="h6" gutterBottom> Partners</Button></div>
                 </Grid>
                 <Grid item xs={12} sm={6} md={3}>
-                    <Typography className='pb-5 capitalize' varient="h6"> Solutions</Typography>
+                    <Typography className='pb-5 capitalize' style={{ fontSize: "2rem" }} varient="h2"> Solutions</Typography>
                     <div> <Button className='pb-5 capitalize' variant="h6" gutterBottom> Marketing</Button></div>
                     <div><Button className='pb-5 capitalize' variant="h6" gutterBottom> Analytics</Button></div>
                     <div> <Button className='pb-5 capitalize' variant="h6" gutterBottom> Commerce</Button></div>
@@ -27,12 +27,12 @@ const Footer = () => {
 
                 </Grid>
                 <Grid item xs={12} sm={6} md={3}>
-                    <Typography className='pb-5 capitalize' varient="h6"> Documentation</Typography>
+                    <Typography className='pb-5 capitalize' style={{ fontSize: "2rem" }} varient="h2"> Documentation</Typography>
                     <div> <Button className='pb-5 capitalize' variant="h6" gutterBottom> Guides</Button></div>
                     <div><Button className='pb-5 capitalize' variant="h6" gutterBottom> Api Status</Button></div>
                 </Grid>
                 <Grid item xs={12} sm={6} md={3}>
-                    <Typography className='pb-5 text-capitalize' varient="h6"> Legal</Typography>
+                    <Typography className='pb-5 text-capitalize' style={{ fontSize: "2rem" }} varient="h3"> Legal</Typography>
                     <div> <Button className='pb-5 text-capitalize' variant="h6" gutterBottom> Claim</Button></div>
                     <div> <Button className='pb-5 text-capitalize' variant="h6" gutterBottom> Privacy</Button></div>
                     <div> <Button className='pb-5 text-capitalize' variant="h6" gutterBottom> Terms</Button></div>
